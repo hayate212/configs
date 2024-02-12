20dcc5192e9ac2f69922e43b26cd048c72defa0a
hayate212 <hayate.JvN@gmail.com>
2024-02-13 01:23:28 +0900

    improve: OS別に設定値を変更

--- lua/extensions/toggleterm.lua
+++ lua/extensions/toggleterm.lua
@@ -1,5 +1,13 @@
-require('toggleterm').setup {
-  shell = 'bash'
-}
+if vim.fn.has('mac') == 1 then 
+  require('toggleterm').setup {
+    shell = 'fish'
+  }
+end
+
+if vim.fn.has('wsl') == 1 then
+  require('toggleterm').setup {
+    shell = 'bash'
+  }
+end
 
 vim.keymap.set('n', '<Leader>`', ':ToggleTerm<CR>', {})
