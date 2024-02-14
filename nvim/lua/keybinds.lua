b73bcf3a8086c1fa6a2820de376eab743b75ad13
hayate212 <hayate.JvN@gmail.com>
2024-02-14 23:37:13 +0900

    feat: 単語移動キーバインド追加

--- nvim/lua/keybinds.lua
+++ nvim/lua/keybinds.lua
@@ -2,6 +2,7 @@ local keymap = vim.keymap
 
 vim.g.mapleader = ' '
 
+keymap.set({ 'n', 'x' }, '<Space>', '<Nop>')
 keymap.set('n', ';', ':')
 
 -- jj で esc
@@ -13,3 +14,8 @@ keymap.set('n', '<Leader>¥', ':vsplit<Return><C-w>w')
 -- アクティブウィンドウの移動
 keymap.set('', '<Leader>l', '<C-w>l')
 keymap.set('', '<Leader>h', '<C-w>h')
+
+-- ALT+右矢印で次の単語の始まりに移動
+vim.api.nvim_set_keymap('n', '<A-Right>', 'w', {noremap = true, silent = true})
+-- ALT+左矢印で前の単語の始まりに移動
+vim.api.nvim_set_keymap('n', '<A-Left>', 'b', {noremap = true, silent = true})
