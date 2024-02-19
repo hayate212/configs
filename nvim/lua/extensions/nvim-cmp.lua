758625bb8570ca5d44d8a9a435f3670671fb9716
hayate212 <hayate.JvN@gmail.com>
2024-02-20 00:46:05 +0900

    improve: add luasnip config

--- nvim/lua/extensions/nvim-cmp.lua
+++ nvim/lua/extensions/nvim-cmp.lua
@@ -1,3 +1,4 @@
+local luasnip = require('luasnip')
 local cmp = require('cmp')
 local lspkind = require('lspkind')
 
@@ -5,12 +6,35 @@ local map = cmp.mapping
 
 cmp.setup {
   enabled = true, 
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
   mapping = map.preset.insert {
     ['<C-d>'] = map.scroll_docs(-4),
     ['<C-f>'] = map.scroll_docs(4),
     ['<C-.>'] = map.complete(),
     ['<C-e>'] = map.abort(),
     ['<CR>'] = map.confirm { select = false },
+    ['<Tab>'] = map(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
   },
   sources = cmp.config.sources {
     { name = 'nvim_lsp' },
@@ -25,3 +49,4 @@ cmp.setup {
     },
   },
 }
+
