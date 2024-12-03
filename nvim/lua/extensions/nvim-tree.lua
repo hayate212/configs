e730f3333230a3b84a0697fe81774eb752a3d74f
hayate212 <hayate.aso@goinc.jp>
2024-12-03 13:34:22 +0900

    autofocus current file

--- nvim/lua/extensions/nvim-tree.lua
+++ nvim/lua/extensions/nvim-tree.lua
@@ -1,4 +1,8 @@
-require('nvim-tree').setup {}
+require('nvim-tree').setup {
+  update_focused_file = {
+    enable = true,
+  },
+}
 
 vim.api.nvim_set_keymap('n', '<Leader>e', ':NvimTreeToggle<CR>', { silent = true, noremap = true })
 
